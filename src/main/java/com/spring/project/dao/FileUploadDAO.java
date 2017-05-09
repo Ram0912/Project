d27533c50b45d842502696e7d64b10ffff335399
Ramkumar <ramkumar0316@gmail.com>
2017-05-09 18:41:52 +0530

    commit 1

---
+++ src/main/java/com/spring/project/dao/FileUploadDAO.java
@@ -0,0 +1,7 @@
+package com.spring.project.dao;
+
+import com.spring.project.model.UploadFile;
+
+public interface FileUploadDAO {
+	void save(UploadFile uploadFile);
+}
---
+++ src/main/java/com/spring/project/dao/FileUploadDAOImpl.java
@@ -0,0 +1,29 @@
+package com.spring.project.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.project.model.UploadFile;
+
+
+@Repository
+public class FileUploadDAOImpl implements FileUploadDAO {
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public FileUploadDAOImpl() {
+	}
+
+	public FileUploadDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	
+	@Transactional
+	public void save(UploadFile uploadFile) {
+		sessionFactory.getCurrentSession().save(uploadFile);
+	}
+
+}
